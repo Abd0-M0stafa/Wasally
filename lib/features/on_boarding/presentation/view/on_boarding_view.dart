bae089cfba779e979f901775065afc8e43081ca5
Ayman Moamed <darknaya.am@gmail.com>
2024-05-05 02:33:40 +0300

    adding on boarding view body

--- lib/features/on_boarding/presentation/view/on_boarding_view.dart
+++ lib/features/on_boarding/presentation/view/on_boarding_view.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:wasally/core/widgets/gradient_background.dart';
+
+import 'widgets/on_boarding_view_body.dart';
 
 class OnBoardingView extends StatelessWidget {
   const OnBoardingView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return const Scaffold(
+      body: GradientBackground(
+        child: OnBoardingViewBody(),
+      ),
+    );
   }
 }
